3b29db1f4c18424192540d819c880425b1f4b380
Miroslav Misik <mirom73@gmail.com>
2019-02-11 19:06:46 +0100

    finished exercises 1,2

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,17 +4,41 @@ namespace Methods
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //1.Write a code that by given name prints on the console "Hello, <name>!"
             //(for example: "Hello, Peter!").
+            Console.Write("EX1: Enter your name: ");
+            string name = Console.ReadLine();
+            PrintHelloAnyBody(name);
+            EndOfScript();
 
             //2.Create a method GetMax() with two integer(int) parameters, that
-            //returns maximal of the two numbers.Write a program that reads three
+            //returns maximal of the two numbers. Write a program that reads three
             //numbers from the console and prints the biggest of them. Use the
             //GetMax() method you just created. Write a test program that validates
             //that the methods works correctly.
+            Console.WriteLine("EX2: Enter 3 numbers, I will return bigest one.");
+            Console.Write("Enter numer 1: ");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.Write("Enter numer 2: ");
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.Write("Enter numer 3: ");
+            int numberThree = int.Parse(Console.ReadLine());
+            int biggestOne = numberOne;
+            biggestOne = GetMax(numberOne, numberTwo);
+            biggestOne = GetMax(biggestOne, numberThree);
 
+            if (biggestOne >= numberOne && biggestOne >= numberTwo && biggestOne >= numberThree)
+            {
+                Console.WriteLine("The biggest number is " + biggestOne);
+            }
+            else
+            {
+                Console.WriteLine("Something is wrong, returned number {0} is not biggest!!!", biggestOne);
+            }            
+
+            EndOfScript();
             //3.Write a method that returns the English name of the last digit of a
             //given number. Example: for 512 prints "two"; for 1024 -> "four".
 
@@ -62,6 +86,38 @@ namespace Methods
             //13. * Write a method that calculates the product of two polynomials with
             //integer coefficients, for example
             //(3x^2 + x - 3) * (x - 1) = (3x^3 - 2x^2 - 4x + 3).
+        }
+
+        //1.Write a code that by given name prints on the console "Hello, <name>!"
+        //(for example: "Hello, Peter!").
+        static void PrintHelloAnyBody (string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        //2.Create a method GetMax() with two integer(int) parameters, that
+        //returns maximal of the two numbers. Write a program that reads three
+        //numbers from the console and prints the biggest of them. Use the
+        //GetMax() method you just created. Write a test program that validates
+        //that the methods works correctly.
+        static int GetMax(int x, int y)
+        {
+            if (x >= y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+
+
+        //support methods
+        static void EndOfScript ()
+        {
+            Console.WriteLine(new String('#', 80));
+            Console.WriteLine();
         }
     }
 }
